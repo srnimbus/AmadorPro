29d1517d76c89f4e8360b1f1a49d9e7011181cd9
= <=>
2012-03-25 20:05:57 -0300

    Devolve faces config, add auth listener, planeja chamada validação

---
+++ JavaSource/br/com/srnimbus/amadorpro/mvc/listener/AuthListener.java
@@ -0,0 +1,35 @@
+package br.com.srnimbus.amadorpro.mvc.listener;
+
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
+public class AuthListener implements PhaseListener {
+
+	// TODO Integrar com o JAAS.
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void afterPhase(PhaseEvent arg0) {
+		// TODO Auto-generated method stub
+
+		System.out.println("verificar requisição posterior");
+
+	}
+
+	@Override
+	public void beforePhase(PhaseEvent arg0) {
+		// TODO Auto-generated method stub
+
+		System.out.println("chamar validação abstractbean");
+
+		// chamar validação abstractbean
+	}
+
+	@Override
+	public PhaseId getPhaseId() {
+
+		return PhaseId.RESTORE_VIEW;
+	}
+
+}
