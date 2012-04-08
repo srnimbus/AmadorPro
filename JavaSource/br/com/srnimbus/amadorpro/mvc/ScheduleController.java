06c7500d355267ea57f6be6ec4228a413a2797d4
= <=>
2012-04-08 18:36:50 -0300

    Projeto Crescendo...

---
+++ JavaSource/br/com/srnimbus/amadorpro/mvc/ScheduleController.java
@@ -0,0 +1,105 @@
+package br.com.srnimbus.amadorpro.mvc;
+
+import java.util.Date;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+import org.primefaces.event.DateSelectEvent;
+import org.primefaces.event.ScheduleEntryMoveEvent;
+import org.primefaces.event.ScheduleEntryResizeEvent;
+import org.primefaces.event.ScheduleEntrySelectEvent;
+import org.primefaces.model.DefaultScheduleEvent;
+import org.primefaces.model.DefaultScheduleModel;
+import org.primefaces.model.ScheduleEvent;
+import org.primefaces.model.ScheduleModel;
+
+@ManagedBean(name = "scheduleController")
+@SessionScoped
+public class ScheduleController {
+
+	private ScheduleModel eventModel;
+
+	private ScheduleModel lazyEventModel;
+
+	private ScheduleEvent event = new DefaultScheduleEvent();
+
+	private String theme;
+
+	public ScheduleController() {
+		eventModel = new DefaultScheduleModel();
+		eventModel.addEvent(new DefaultScheduleEvent("Champions League Match", new Date(), new Date("04/08/2012")));
+
+	}
+
+	public void addEvent(ActionEvent actionEvent) {
+		if (event.getId() == null)
+			eventModel.addEvent(event);
+		else
+			eventModel.updateEvent(event);
+
+		event = new DefaultScheduleEvent();
+	}
+
+	public void onEventSelect(ScheduleEntrySelectEvent selectEvent) {
+		event = selectEvent.getScheduleEvent();
+	}
+
+	public void onDateSelect(DateSelectEvent selectEvent) {
+		event = new DefaultScheduleEvent("Pelada", selectEvent.getDate(), selectEvent.getDate());
+	}
+
+	public void onEventMove(ScheduleEntryMoveEvent event) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event moved", "Day delta:"
+				+ event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
+
+		addMessage(message);
+	}
+
+	public void onEventResize(ScheduleEntryResizeEvent event) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event resized", "Day delta:"
+				+ event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
+
+		addMessage(message);
+	}
+
+	private void addMessage(FacesMessage message) {
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+
+	public ScheduleModel getEventModel() {
+		return eventModel;
+	}
+
+	public void setEventModel(ScheduleModel eventModel) {
+		this.eventModel = eventModel;
+	}
+
+	public ScheduleModel getLazyEventModel() {
+		return lazyEventModel;
+	}
+
+	public void setLazyEventModel(ScheduleModel lazyEventModel) {
+		this.lazyEventModel = lazyEventModel;
+	}
+
+	public ScheduleEvent getEvent() {
+		return event;
+	}
+
+	public void setEvent(ScheduleEvent event) {
+		this.event = event;
+	}
+
+	public String getTheme() {
+		return theme;
+	}
+
+	public void setTheme(String theme) {
+		this.theme = theme;
+	}
+
+}
